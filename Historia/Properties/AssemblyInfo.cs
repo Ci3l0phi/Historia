41b778150fabe4a7bccec6560baeb60e9593e0ad
Ci3l0phi <khloe@tuta.io>
2017-05-25 04:17:01 -0700

    Removed older proxy implementation

--- Historia/Properties/AssemblyInfo.cs
+++ Historia/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Historia")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Proxy and packet logging tool.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Historia")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.8.0")]
