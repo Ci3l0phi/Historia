b91c993aeb21dfeef6a05fa600a5a3e17704d82b
Ci3l0phi <khloe@tuta.io>
2017-05-25 13:07:05 -0700

    Fixed issue where paths for includes not found.

--- Historia/HTMLWriter.cs
+++ Historia/HTMLWriter.cs
@@ -14,25 +14,28 @@ namespace Historia
         public string docPath { get; private set; }
         private HtmlDocument doc;
 
-        private static readonly string packetsPath = "packets";
-        private static readonly string includesPath = "includes";
+        private static string packetsPath;
+        private static string includesPath;
         private static readonly List<string> includes = new List<string> { "Template.css", "Template.html" };
+        private static readonly string executableDir = AppDomain.CurrentDomain.BaseDirectory;
 
         public HTMLWriter() : this(DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".html") {}
 
         public HTMLWriter(string filename)
         {
-            Console.WriteLine("[HTMLWriter] Initializing output directories.");
             try
             {
-                Directory.CreateDirectory(packetsPath);
-                Directory.CreateDirectory(Path.Combine(packetsPath, includesPath));
-                foreach (var include in includes)
-                    File.Copy(include, Path.Combine(packetsPath, includesPath, include), true);
+                Console.WriteLine("[HTMLWriter] Initializing output directories.");
+                packetsPath = Path.Combine(executableDir, "packets");
+                includesPath = Path.Combine(packetsPath, "includes");
 
+                Directory.CreateDirectory(packetsPath);
+                Directory.CreateDirectory(includesPath);
+                foreach (var include in includes)
+                    File.Copy(Path.Combine(executableDir, include), Path.Combine(includesPath, include), true);
 
                 doc = new HtmlDocument();
-                doc.Load(Path.Combine(packetsPath, includesPath, "Template.html"));
+                doc.Load(Path.Combine(includesPath, "Template.html"));
                 this.docPath = Path.Combine(packetsPath, filename);
                 doc.Save(this.docPath);
             } catch (Exception e)
@@ -65,7 +68,7 @@ namespace Historia
             {
                 Console.WriteLine(e.Message);
             }
-            
+
         }
 
     }
